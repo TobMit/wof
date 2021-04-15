ad30e834f5b26d1d1aa7c1e22e3e6beaccc39362
TobMit <tobik.m1@gmail.com>
2021-04-15 08:59:49 +0200

    Uprava questovych predmetov. (presun spawnowanie predmetov)

---
+++ src/sk/uniza/fri/wof/hra/questy/PredmetSpawner.java
@@ -0,0 +1,26 @@
+package sk.uniza.fri.wof.hra.questy;
+
+import sk.uniza.fri.wof.hra.Hrac;
+import sk.uniza.fri.wof.prostredie.predmety.Predmet;
+import sk.uniza.fri.wof.prostredie.predmety.QuestovyPredmet;
+
+public class PredmetSpawner {
+    private final String nazovMiestnosti;
+    private final QuestovyPredmet predmet;
+    private boolean bolSpawnuty;
+
+    public PredmetSpawner(String nazovMiestnosti, QuestovyPredmet predmet) {
+        this.nazovMiestnosti = nazovMiestnosti;
+        this.predmet = predmet;
+        this.bolSpawnuty = false;
+    }
+
+    public void spavniAkTreba(Hrac hrac) {
+        if (hrac.getAktualnaMiestnost().getPopis().startsWith(this.nazovMiestnosti)) {
+            if (!this.bolSpawnuty) {
+                hrac.getAktualnaMiestnost().polozPredmet(this.predmet);
+                this.bolSpawnuty = true;
+            }
+        }
+    }
+}
--- src/sk/uniza/fri/wof/hra/questy/QuestPrezentacia.java
+++ src/sk/uniza/fri/wof/hra/questy/QuestPrezentacia.java
@@ -5,27 +5,28 @@ import sk.uniza.fri.wof.prostredie.predmety.QuestovyPredmet;
 
 public class QuestPrezentacia extends Quest implements IQuestKontrolaMiestnosti, IquestKontrolaPredmet {
     private final String quest;
+    private final PredmetSpawner usbSpawner;
     private boolean bolUsbSpawnuty;
 
 
     public QuestPrezentacia(String quest) {
         this.quest = quest;
         this.bolUsbSpawnuty = false;
+        this.usbSpawner = new PredmetSpawner("ic", new QuestovyPredmet("usb"));
     }
 
     @Override
     public void hracVosielDoMiestnosti(Hrac hrac) {
-        if (hrac.getAktualnaMiestnost().getPopis().startsWith("RA006")) {
+        this.usbSpawner.spavniAkTreba(hrac);
+        //---skontrolovať od ucitela
+        if (hrac.getAktualnaMiestnost().getPopis().startsWith("IC")) {
             if (hrac.getMaPredmet("usb")) {
                 System.out.println("Ok, tak mozes spustit prezentaciu");
             } else {
                 System.out.println("Ved si si zabudol prezentaciu");
             }
         } else if (hrac.getAktualnaMiestnost().getPopis().startsWith("IC")) {
-            if (!this.bolUsbSpawnuty) {
-                hrac.getAktualnaMiestnost().polozPredmet(new QuestovyPredmet("usb"));
-                this.bolUsbSpawnuty = true;
-            }
+            this.usbSpawner.spavniAkTreba(hrac);
         }
     }
 
